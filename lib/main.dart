3be9457e89673a97e778ebfbdb082ed30fece944
aniketaniket <aniketambore0@gmail.com>
2020-10-02 18:12:35 +0530

    login_screen_1.dart overflow issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_screens/misc/slide_list_view.dart';
 
+import 'login_screen_1.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
