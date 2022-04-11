5ec5008914f6c0e0c7b7a5170938618aa04e969a
zimvo <zimvo@busymed.co.za>
2022-04-11 19:29:29 +0200

    adjusted test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_login_screens/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_screens/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
